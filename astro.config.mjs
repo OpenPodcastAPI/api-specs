22ad3f9b3dc8711dba2fca918a955d421e840df4
Ciarán Ainsworth <ciaranainsworth@gmail.com>
2023-08-27 16:07:34 +0200

    feat: auto-import components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import AutoImport from "astro-auto-import";
 import {
 	remarkDefinitionList,
 	defListHastHandlers,
@@ -7,30 +8,41 @@ import {
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [starlight({
-		title: "Open Podcast API",
-		favicon: "favicon.ico",
-		social: {
-			github: "https://github.com/OpenPodcastApi/api-specs"
-		},
-		sidebar: [{
-			label: "Project overview",
-			link: "overview"
-		}, {
-			label: "Specifications",
-			items: [{ label: "Introduction", link: "specs" }, {
-				label: "Subscriptions",
-				collapsed: true,
-				autogenerate: {
-					directory: "specs/subscriptions"
-				}
-			}]
-		},
-		{
-			label: "API explorer",
-			link: "rapidoc.html"
-		}]
-	})],
+	integrations: [
+		AutoImport({
+			imports: [
+				{ "@astrojs/starlight/components": ["Card", "CardGrid", "LinkCard", "Tabs", "TabItem"] }
+			]
+		}),
+		starlight({
+			title: "Open Podcast API",
+			favicon: "favicon.ico",
+			social: {
+				github: "https://github.com/OpenPodcastApi/api-specs"
+			},
+			sidebar: [{
+				label: "Project overview",
+				link: "overview"
+			}, {
+					label: "Specifications",
+					items: [
+						{
+							label: "Introduction",
+							link: "specs"
+						},
+						{
+							label: "Subscriptions",
+							collapsed: true,
+							autogenerate: {
+								directory: "specs/subscriptions"
+							}
+						}]
+				},
+				{
+					label: "API explorer",
+					link: "rapidoc.html"
+				}]
+		})],
 	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
 	image: {
 		service: {
