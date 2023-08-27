f30abb944ceb666e5a3b6e05286bf20b6eba3989
Ciarán Ainsworth <ciaranainsworth@gmail.com>
2023-08-27 23:56:34 +0200

    feat: add initial versioning endpoints

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,6 +45,13 @@ export default defineConfig({
               link: "specs",
             },
             {
+              label: "Versioning",
+              collapsed: true,
+              autogenerate: {
+                directory: "specs/versioning",
+              },
+            },
+            {
               label: "Subscriptions",
               collapsed: true,
               autogenerate: {
