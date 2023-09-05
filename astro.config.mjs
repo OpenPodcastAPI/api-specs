3d0a9f9fccc12d3b6d3d277b34c46806bd690810
Ciarán Ainsworth <sporiff@tilde.club>
2023-09-06 01:18:29 +0200

    fix: correct sidebar order

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,68 +35,6 @@ export default defineConfig({
               link: "specs",
             },
             {
-              label: "Subscriptions",
-              collapsed: true,
-              autogenerate: {
-                directory: "specs/subscriptions",
-              },
-            },
-          ],
-        },
-        {
-          label: "API explorer",
-          link: "rapidoc.html",
-        },
-      ],
-    }),
-    AutoImport({
-      imports: [
-        {
-          "@astrojs/starlight/components": [
-            "Card",
-            "CardGrid",
-            "LinkCard",
-            "Tabs",
-            "TabItem",
-          ],
-        },
-      ],
-    }),
-  ],
-  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/sharp",
-    },
-  },
-  markdown: {
-    remarkPlugins: [remarkDefinitionList],
-    remarkRehype: {
-      handlers: {
-        ...defListHastHandlers,
-      },
-    },
-  },
-  integrations: [
-    starlight({
-      title: "Open Podcast API",
-      favicon: "favicon.ico",
-      social: {
-        github: "https://github.com/OpenPodcastApi/api-specs",
-      },
-      sidebar: [
-        {
-          label: "Project overview",
-          link: "about",
-        },
-        {
-          label: "Specifications",
-          items: [
-            {
-              label: "Introduction",
-              link: "specs",
-            },
-            {
               label: "Versioning",
               collapsed: true,
               autogenerate: {
