bef364505114f9cc73ceca546a9d055efa48a354
Ciarán Ainsworth <cda@sporiff.dev>
2024-01-28 16:31:00 +0100

    Add CoC to main site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,59 +2,76 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import AutoImport from "astro-auto-import";
 import {
-	remarkDefinitionList,
-	defListHastHandlers,
+  remarkDefinitionList,
+  defListHastHandlers,
 } from "remark-definition-list";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: "Open Podcast API",
-			favicon: "favicon.ico",
-			social: {
-				github: "https://github.com/OpenPodcastApi/api-specs"
-			},
-			sidebar: [{
-				label: "Project overview",
-				link: "about"
-			}, {
-				label: "Specifications",
-				items: [
-					{
-						label: "Introduction",
-						link: "specs"
-					},
-					{
-						label: "Subscriptions",
-						collapsed: true,
-						autogenerate: {
-							directory: "specs/subscriptions"
-						}
-					}]
-			},
-			{
-				label: "API explorer",
-				link: "rapidoc.html"
-			}]
-		}),
-		AutoImport({
-			imports: [
-				{ "@astrojs/starlight/components": ["Card", "CardGrid", "LinkCard", "Tabs", "TabItem"] }
-			]
-		})],
-	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-	image: {
-		service: {
-			entrypoint: "astro/assets/services/sharp"
-		}
-	},
-	markdown: {
-		remarkPlugins: [remarkDefinitionList],
-		remarkRehype: {
-			handlers: {
-				...defListHastHandlers,
-			},
-		},
-	}
+  integrations: [
+    starlight({
+      title: "Open Podcast API",
+      favicon: "favicon.ico",
+      social: {
+        github: "https://github.com/OpenPodcastApi/api-specs",
+      },
+      sidebar: [
+        {
+          label: "Project overview",
+          link: "about",
+        },
+        {
+          label: "Code of conduct",
+          link: "coc",
+        },
+        {
+          label: "Specifications",
+          items: [
+            {
+              label: "Introduction",
+              link: "specs",
+            },
+            {
+              label: "Subscriptions",
+              collapsed: true,
+              autogenerate: {
+                directory: "specs/subscriptions",
+              },
+            },
+          ],
+        },
+        {
+          label: "API explorer",
+          link: "rapidoc.html",
+        },
+      ],
+    }),
+    AutoImport({
+      imports: [
+        {
+          "@astrojs/starlight/components": [
+            "Card",
+            "CardGrid",
+            "LinkCard",
+            "Tabs",
+            "TabItem",
+          ],
+        },
+      ],
+    }),
+  ],
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
+  markdown: {
+    remarkPlugins: [remarkDefinitionList],
+    remarkRehype: {
+      handlers: {
+        ...defListHastHandlers,
+      },
+    },
+  },
 });
