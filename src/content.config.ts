18dd54e8dc8307171b32c90386828af2ef05228e
Ciarán Ainsworth <cda@sporiff.dev>
2026-03-12 17:31:48 +0100

    Upgrade all Astro deps

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { docsLoader } from '@astrojs/starlight/loaders';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
